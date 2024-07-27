33b3dc800c1ccc34158290412843e9dbc2a78515
Jean Loui Bernard <45553309+JeanExtreme002@users.noreply.github.com>
2024-07-27 05:52:38 +0000

    improved header text

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Jean Loui Bernard',
     title: "Software Engineer",
-    description1: "In this profile you will find a highly skilled and professional software engineer with a proven track record of success.",
+    description1: "On this page you will find a highly skilled and professional software engineer with a proven track record of success.",
     description2: "My professional goal is working on challenging projects that make a meaningful impact to the world, applying my skills in development, algorithms, data structures and optimization. I am open to exploring different paths and am eager to take the next step in my professional journey.",
     image: './picture.jpeg',
     resumePdf: resume
