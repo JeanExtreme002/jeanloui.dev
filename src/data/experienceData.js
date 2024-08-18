1c0d0f255843bf160ab6d6c074c698d1f6f7f39f
Jean Loui Bernard <45553309+JeanExtreme002@users.noreply.github.com>
2024-08-18 03:07:05 +0000

    update

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,20 +3,27 @@ export const experienceBio = "Forward strides in my career shape my path toward 
 export const experienceData = [
     {
         id: 1,
-        company: 'Federal University of Bahia',
-        jobtitle: 'Researcher in Neural Radiance Fields (NeRF)',
-        startYear: '2023',
+        company: 'Jusbrasil',
+        jobtitle: 'Software Enginner Intern',
+        startYear: '2024',
         endYear: 'Present'
     },
     {
         id: 2,
+        company: 'Federal University of Bahia',
+        jobtitle: 'Researcher in Neural Radiance Fields (NeRF)',
+        startYear: '2023',
+        endYear: '2024'
+    },
+    {
+        id: 3,
         company: 'InfoJR - SEMCOMP23',
         jobtitle: 'Python Workshop Leader',
         startYear: '2023',
         endYear: '2023'
     },
     {
-        id: 3,
+        id: 4,
         company: 'Escavador',
         jobtitle: 'Software Engineer Intern',
         startYear: '2022',
